d32e02b8347fdc04911177533e47819d8ad6020b
Nate Florencio <nflorencio@showcasepro.com.br>
2023-11-06 13:57:57 -0300

    Edit Answer Use Case.

---
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -0,0 +1,54 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { EditAnswerUseCase } from './edit-answer'
+import { makeAnswer } from 'test/factories/make-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: EditAnswerUseCase
+
+describe('Edit Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to edit a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      authorId: 'author-1',
+      content: 'Conteúdo teste',
+      answerId: newAnswer.id.toValue(),
+    })
+
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: 'Conteúdo teste',
+    })
+  })
+
+  it('should not be able to edit a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await expect(() => {
+      return sut.execute({
+        authorId: 'author-2',
+        content: 'Conteúdo teste',
+        answerId: newAnswer.id.toValue(),
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
