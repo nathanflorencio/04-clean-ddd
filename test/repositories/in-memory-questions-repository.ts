83e347853bba32c8e9e48fb963463f5ef7ec97dd
Nate Florencio <nflorencio@showcasepro.com.br>
2023-11-08 16:07:12 -0300

    Subdomains & Domain Events.

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
@@ -38,14 +39,18 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return questions
   }
 
+  async create(question: Question) {
+    this.items.push(question)
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
+  }
+
   async save(question: Question) {
     const itemIndex = this.items.findIndex((item) => item.id === question.id)
 
     this.items[itemIndex] = question
-  }
 
-  async create(question: Question) {
-    this.items.push(question)
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async delete(question: Question) {
